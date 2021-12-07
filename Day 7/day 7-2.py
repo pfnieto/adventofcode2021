fb2da79bf3ccbc5f0001dc2979fab59b76b05253
unknown <pfnieto@uc.cl>
2021-12-07 14:05:19 -0300

    Day 7

---
+++ Day 7/day 7-2.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  7 13:58:38 2021
+
+@author: Pablo
+"""
+
+def read_input(filename):
+    with open(filename) as f:
+        fuel = {}
+        original = [int(x) for x in f.readline().split(",")]
+        count = [0] * (max(original) + 1)
+        total = 0
+        for x in original:
+            count[x] += 1
+            total += x
+        for i in range(len(count)):
+            for j in range(len(count)):
+                if i == j:
+                    continue
+                if i not in fuel:
+                    fuel[i] = 0
+                fuel[i] += (abs(i - j) * (1 + abs(i - j)) * count[j])/2
+            if i > 1 and fuel[i] > fuel[i - 1]:
+                break
+        return fuel
+    
+fuel = read_input("input 7.txt")
+out = min(fuel.values())
+            
