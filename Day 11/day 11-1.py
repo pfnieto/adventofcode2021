74a6f06684fc1b2239f545fc8e3e6aa16652f223
unknown <pfnieto@uc.cl>
2021-12-11 11:37:34 -0300

    Day 11

---
+++ Day 11/day 11-1.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 11 10:10:02 2021
+
+@author: Pablo
+"""
+
+def read_input(filename):
+    values = []
+    with open(filename) as f:
+        for line in f:
+            values.append([int(x) for x in line.strip()])
+    return values
+
+def reset(values):
+    count = 0
+    for y in range(len(values)):
+        for x in range(len(values)):
+            if values[y][x] >= 10:
+                count += 1
+                values[y][x] = 0
+    return count
+
+def travel_path(x, y, values):
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if (x + i < 0 or x + i >= len(values[0]) or y + j < 0 or y + j >= len(values)):
+                continue
+            values[y + j][x + i] += 1
+            if values[y + j][x + i] == 10:
+                travel_path(x + i, y + j, values)
+
+def next_step(values):
+    for y in range(len(values)):
+        for x in range(len(values)):
+            values[y][x] += 1
+            if values[y][x] == 10:
+                travel_path(x, y, values)
+
+values = read_input("input 11.txt")
+count = 0
+for step in range(100):
+    next_step(values)
+    count += reset(values)
